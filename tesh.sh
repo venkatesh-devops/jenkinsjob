5e9f4e410b7e16b967cd63b86507ddc09c037c6c
venkatesh <nvenkat285@gmail.com>
2021-09-25 17:30:17 +0530

    Update tesh.sh

--- tesh.sh
+++ tesh.sh
@@ -5,4 +5,3 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
-service jenkins status
