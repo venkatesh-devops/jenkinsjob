9523c4859231ce1f930f6763bd990c195ed6ed5d
venkatesh <nvenkat285@gmail.com>
2021-09-26 21:18:07 +0530

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-touch /etc/apt/sources.list.d/kubernetes.list
-echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >>/etc/apt/sources.list.d/kubernetes.list
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+sudo echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >>/etc/apt/sources.list.d/kubernetes.list
 #cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  deb http://apt.kubernetes.io/ kubernetes-xenial main  EOF
 apt-get update
 apt-get install -y kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 docker.io
