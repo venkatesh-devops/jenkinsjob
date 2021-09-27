3f9f5a5212dbb5aed054105e7a59d8036a4bcba5
venkatesh <nvenkat285@gmail.com>
2021-09-27 19:42:37 +0530

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -8,5 +8,6 @@ sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc
 #echo "The page was created by the user-data" | sudo tee /var/www/html/index.html
 #cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  deb http://apt.kubernetes.io/ kubernetes-xenial main  EOF
 sudo apt-get update
-sudo apt-get install -y kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 docker.io
+sudo apt-get install -y docker.io
+sudo apt-get install -y kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00
 kubeadm init
