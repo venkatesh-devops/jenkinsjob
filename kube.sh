eb9436d335b1f007e63c9d4ace9f595a7aa81770
venkatesh <nvenkat285@gmail.com>
2021-09-27 19:36:05 +0530

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -2,7 +2,7 @@
 sudo apt-get update  
 sudo apt-get install -y apt-transport-https
 sudo apt-get install curl -y
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 #echo "The page was created by the user-data" | sudo tee /var/www/html/index.html
