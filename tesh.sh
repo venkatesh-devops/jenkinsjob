fa958588eed6481d72b6e18e39bc5ecf77941886
venkatesh <nvenkat285@gmail.com>
2021-10-28 14:29:44 +0530

    Update tesh.sh

--- tesh.sh
+++ tesh.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 sudo apt-get update
 sudo apt-get install openjdk-8-jre openjdk-8-jdk -y
+sudo apt-get wget
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
